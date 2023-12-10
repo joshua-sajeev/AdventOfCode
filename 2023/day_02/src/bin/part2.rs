7d5038417f2e81590189443df95a0b8226ca7bdf
Joshua Sajeev <joshuasajeevnv@gmail.com>
2023-12-10 20:45:50 +0530

    day2_part2

---
+++ 2023/day_02/src/bin/part2.rs
@@ -0,0 +1,55 @@
+use std::fs;
+
+fn main() {
+    let mut input = fs::read_to_string("input2.txt")
+        .expect("Should have been able to read the file");
+
+    input = input.replace("Game","").replace(",","").replace(":","");
+    let mut lines : Vec<_> = input.split('\n').collect();
+    lines.remove(lines.len()-1);
+    let mut total = 0;
+    for i in 0..lines.len(){
+        println!("{}",lines[i]);
+        let line: Vec<_> = lines[i].split(';').collect();
+        let mut mb_v = 0;
+        let mut mg_v = 0;
+        let mut mr_v = 0;
+        let mut b_v = 0;
+        let mut r_v = 0;
+        let mut g_v = 0;
+        let mut sum = 0;
+        for j in 0..line.len(){
+            let words : Vec<_> = line[j].split(' ').collect();
+            for k in 1..words.len(){
+                let prev = k-1;
+                if words[k].contains("blue"){
+                    b_v = words[prev].parse::<i32>().unwrap();
+                }
+
+                if words[k].contains("green"){
+                    g_v = words[prev].parse::<i32>().unwrap();
+                }
+
+                if words[k].contains("red"){
+                    r_v = words[prev].parse::<i32>().unwrap();
+                }
+
+                if b_v > mb_v {
+                    mb_v = b_v;
+                } 
+
+                if r_v > mr_v {
+                    mr_v = r_v;
+                } 
+                
+                if g_v > mg_v {
+                    mg_v = g_v;
+                } 
+            }
+        }
+        sum = sum + (mb_v * mr_v * mg_v);
+        println!("{sum}");
+        total = total + sum;
+    }
+    println!("{total}");
+}
